d7dd474ef17319528f1305e9c6828895383e6980
TondraMou <tondramou3@gmail.com>
2024-12-07 16:24:38 +0600

    added post method

--- index.js
+++ index.js
@@ -27,12 +27,27 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const database = client.db('visaDB');
+    const visaCollection = database.collection('visa');
+
+
+    // Add Visa Route
+    app.post('/visa', async(req, res) => {
+      const newVisa = req.body;
+      
+      const result = await visaCollection.insertOne(newVisa);
+      res.send(result);
+    });
+    
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    
   }
 }
 run().catch(console.dir);
